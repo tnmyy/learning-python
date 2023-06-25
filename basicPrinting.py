340bd53e44b1ce5dbd88c94531bcdd29f06c9d24
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-06-25 17:43:30 +0530

    Day 1 Coding

---
+++ basicPrinting.py
@@ -0,0 +1,24 @@
+# This is a single line comment
+"""This 
+is 
+a 
+multi
+line
+comment"""
+
+print("I am Tanmay Upreti")
+
+# Escape Sequence Characters
+
+# \n = New line
+
+print("I am Tanmay\nI like programming")
+
+# \" = New character
+print('I am "Tanmay Upreti"')
+
+# sep = Separated the contents in a string
+# end = The value before the next print
+print(9, "T", 69, sep="~", end="Yo\n")
+
+print("Hey", 990, 00)
