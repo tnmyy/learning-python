f0f27fb3a0d6ff31718481154368e34cf891f04e
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-06-26 14:51:15 +0530

    Variables & Data Types

---
+++ variableAndDataTypes.py
@@ -0,0 +1,37 @@
+# Variable In Python
+
+a = 6969696969
+print(a)
+
+b = "TNMY"
+print(b)
+
+c = True
+print(c)
+
+d = None
+print(d)
+
+# Data Types in Python
+
+print(type(a))  # Number
+print(type(b))  # String
+print(type(c))  # Boolean
+print(type(d))  # None
+
+e = 69.69
+print(type(e))  # Float
+
+f = (8, 1)
+print(type(f))  # Complex Number/Tuple (Real, Imaginary)
+
+
+list = [69, 96, "Tanmay", None, 9.2, (8.09), [1, 2, 3]]
+print(type(list))  # list - mutable
+
+tuple = (69, 96, "Tanmay", None, 9.2, (8.09))
+print(type(tuple))  # tuple - immutable
+
+dictionary = {"Name": "Tanmay", "Percentage": 94.6}
+print(type(dictionary))
+print(dictionary)
